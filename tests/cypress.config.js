22a9d76b1bbdc92c915c55d15070523787e53f7d
Janin-Michel-Mathias <micheljanin296@gmail.com>
2025-02-05 14:24:01 +0100

    Fix lint

--- tests/cypress.config.js
+++ tests/cypress.config.js
@@ -1,8 +1,9 @@
-const { defineConfig } = require("cypress");
+import { defineConfig } from "cypress";
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: "http://localhost",
+     //eslint-disable-next-line
     setupNodeEvents(on, config) {
     },
   },
